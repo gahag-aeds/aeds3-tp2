9b0eb65f5aaafc1202bec49fe2dc838efd325b4a
gahag <gabriel.s.b@live.com>
2017-11-14 02:05:44 -0200

    Added benchmark data files. Updated test framework. Updated test script.

--- tests/test.py
+++ tests/test.py
@@ -9,16 +9,17 @@ tests_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 tptester.tp_test(
-  [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
+  #[ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
+  [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 ],
   
   os.getcwd() + '/' + sys.argv[1],
   
   #valgrind = True,
   
-  #graph = True,
+  graph = True,
   
-  input_file = lambda ix:  tests_dir + '/data/toys/' + str(ix) + '.in',
-  answer_file = lambda ix: tests_dir + '/data/toys/' + str(ix) + '.out',
+  input_file = lambda ix:  tests_dir + '/data/bench/' + str(ix) + '.in',
+  answer_file = lambda ix: None, #tests_dir + '/data/toys/' + str(ix) + '.out',
   
   args = lambda ix: [ ],
   stdin = lambda ifile: ifile,
